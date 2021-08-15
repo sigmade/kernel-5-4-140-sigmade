b5b58cc0500b4332abe941df2c3a8ac8055c0be3
Egor <55326490+sigmade@users.noreply.github.com>
2021-08-15 07:40:10 +0600

    Update install-kernel.sh

--- install-kernel.sh
+++ install-kernel.sh
@@ -3,6 +3,8 @@
 cd /usr/src
 wget https://sourceforge.net/projects/kernel-5-4-140-sigmade/files/linux-kernel-5.4.140-sigmade.tar/download -O kernel.tar
 tar -xvf kernel.tar
-dpkg - i headers.deb image.deb libc-dev.deb
+dpkg -i headers.deb image.deb libc-dev.deb
 rm kernel.tar
+
+#Check
 uname -r
